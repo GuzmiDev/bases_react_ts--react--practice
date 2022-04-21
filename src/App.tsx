b872e360299d3b25c506bcff258f948f8aaef64c
Erick Guzman <guzmi.dev@gmail.com>
2022-04-21 13:55:34 -0600

    :art: useReducer TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { Usuario } from "./components/Usuario";
 import { TimerPadre } from "./components/TimerPadre";
+import { ContadorRed } from "./components/ContadorRed";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
       <h2>useEffect - useRef</h2>
       <hr />
       <TimerPadre />
+      <h2>useReducer</h2>
+      <hr />
+      <ContadorRed />
     </>
   );
 }
